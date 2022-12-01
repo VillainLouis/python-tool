73c445b160e4881bc2b0ef767c2e4bf1b021ec81
VillainLouis <115220933+VillainLouis@users.noreply.github.com>
2022-12-01 14:30:42 +0800

    Create ssh_jetson.py

---
+++ ssh_jetson.py
@@ -0,0 +1,14 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="ssh corresponding jetson device.")
+
+parser.add_argument('--device', type=str, default='')
+
+args = parser.parse_args()
+if 'edge' in args.device:
+    os.system('ssh %s@192.168.1.%02d' % (args.device, int(args.device[-2:])+10))
+elif 'agx' in args.device:
+    os.system('ssh %s@192.168.1.%02d' % (args.device, int(args.device[-2:])+80))
+elif 'nx' in args.device:
+    os.system('ssh %s@192.168.1.%02d' % (args.device, int(args.device[-2:])+40))
